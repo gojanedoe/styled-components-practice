1b237e64670591d6cbb7c124f5e2048d2eb9025c
Jane Doe <77185646+gojanedoe@users.noreply.github.com>
2021-08-25 21:04:13 -0400

    Add ability for button to be changed to different colors

--- src/Components/Button.style.js
+++ src/Components/Button.style.js
@@ -3,5 +3,5 @@ import styled from "styled-components";
 export const Button = styled.button`
   width: 200px;
   height: 50px;
-  background-color: red;
+  background-color: ${(props) => props.backgroundColor};
 `;
