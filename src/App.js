c224a8af8ad2e8145aceddcd8c0e7e9f640d659c
Jane Doe <77185646+gojanedoe@users.noreply.github.com>
2021-09-01 15:51:02 -0400

    Create global styles

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import { StyledButton } from "./Components/Button.style";
 import { AppContainer } from "./Components/Container.style";
+import { GlobalStyles } from "./GlobalStyles.style";
 
 function App() {
   return (
     <AppContainer>
+      <GlobalStyles />
       <StyledButton
         buttonLabel="Click Here"
         backgroundColor="violet"
