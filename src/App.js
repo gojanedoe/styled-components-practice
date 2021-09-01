f79a244645caadc7c2ca7157dfc02e23137fd9dc
Jane Doe <77185646+gojanedoe@users.noreply.github.com>
2021-09-01 15:36:33 -0400

    Separate out button component from styled component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
-import { Button, ButtonLabel } from "./Components/Button.style";
+import { StyledButton } from "./Components/Button.style";
 import { AppContainer } from "./Components/Container.style";
 
 function App() {
   return (
     <AppContainer>
-      <Button backgroundColor="red">
-        <ButtonLabel>Click this button</ButtonLabel>
-      </Button>
-      <Button backgroundColor="violet">Click this button</Button>
+      <StyledButton
+        buttonLabel="Click Here"
+        backgroundColor="violet"
+      ></StyledButton>
     </AppContainer>
   );
 }
